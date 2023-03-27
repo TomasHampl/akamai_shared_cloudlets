982b2884adff448a04a32e10b5e847f9469a9fc7
TomasHampl <tomas.hampl@gmail.com>
2023-03-27 23:04:50 +0200

    Updated test path

--- tests/test_api_requests.py
+++ tests/test_api_requests.py
@@ -1,7 +1,7 @@
 import os
 import unittest
 
-from src import akamai_http_requests_wrapper as wrapper
+from ..src import akamai_http_requests_wrapper as wrapper
 
 
 class TestAkamaiRequestWrapper(unittest.TestCase):
